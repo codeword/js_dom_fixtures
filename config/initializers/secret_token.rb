22e8b082d918c568cf1195bc1924d9dbda782fe7
Jonathan Barnes <pair+jonathan@pivotallabs.com>
2013-03-31 12:34:55 -0700

    Reset to vanilla Rails3 install using rspec2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsDomFixtures::Application.config.secret_token = 'bfc52104e64ab8ee5fbee105fd39cbc0162c480397e789c6f8d6de4a9f30892ff40636a76d0029b491defa841ee1777d75107ee5a9ec2f549abc9d19aeef037c'
